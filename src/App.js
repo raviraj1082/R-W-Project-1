1af82ccfba47a107ec53a9070043787456710ad7
raviraj1082 <ravijethva3@gmail.com>
2021-08-25 23:36:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Datatable from "./Componets/Datatable";
+import Timer from "./Containers/Timer";
+
+function App() {
+  return (
+   <>
+     <Datatable/>
+     <Timer/>
+   </>
+  );
+}
+
+export default App;
